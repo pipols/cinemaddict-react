19961b6a2363474dff45ae3c26d56b0a0f93ae9a
pipols <pipols2007@gmail.com>
2020-06-07 20:10:09 +0300

    логика переключения сортировки в меню

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -1,13 +1,59 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getActiveSort } from "../../selector";
+import { SortType } from "../../const";
+import { ActionCreator } from "../../reducer";
 
-const Sort = () => {
+const ACTIVE_CLASS_NAME = `sort__button--active`;
+
+const Sort = ({ activeSort, setActiveSort }) => {
   return (
     <ul className="sort">
-      <li><a href="#" className="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" className="sort__button">Sort by date</a></li>
-      <li><a href="#" className="sort__button">Sort by rating</a></li>
+      <li>
+        <a
+          onClick={() => setActiveSort(SortType.DEFAULT)}
+          href="#"
+          className={`sort__button ${
+            activeSort === SortType.DEFAULT ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
+          Sort by default
+        </a>
+      </li>
+      <li>
+        <a
+          onClick={() => setActiveSort(SortType.DATE)}
+          href="#"
+          className={`sort__button ${
+            activeSort === SortType.DATE ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
+          Sort by date
+        </a>
+      </li>
+      <li>
+        <a
+          onClick={() => setActiveSort(SortType.RATING)}
+          href="#"
+          className={`sort__button ${
+            activeSort === SortType.RATING ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
+          Sort by rating
+        </a>
+      </li>
     </ul>
   );
 };
 
-export default Sort;
+const mapStateToProps = (state) => ({
+  activeSort: getActiveSort(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setActiveSort(sortType) {
+    dispatch(ActionCreator.setSortType(sortType));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sort);
