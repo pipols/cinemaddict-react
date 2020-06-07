9bddbdfa84cfceebc7d27ff7399a0e957b79afb3
pipols <pipols2007@gmail.com>
2020-06-07 18:21:23 +0300

    selector сортировки фильмов

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,18 @@
 import { getCardsByFilter } from "./utils/filter";
 import { getCardsBySortType } from "./utils/sort";
+import { createSelector } from "reselect";
 
 export const getFilms = (state) => state.films;
 export const getOpenedFilm = (state) => state.openedFilm;
+export const getActiveSort = (state) => state.activeSortType;
+export const getActiveFilter = (state) => state.activeFilterType;
+
+export const getFilteredFilmsSelector = createSelector(
+  getFilms,
+  getActiveSort,
+  getActiveFilter,
+  (films, activeSort, activeFilter) => {
+    const filteredFilms = getCardsByFilter(films, getActiveFilter);
+    return getCardsBySortType(filteredFilms, getActiveSort);
+  }
+);
