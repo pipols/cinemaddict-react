0aa5b528a2aeff4c9eda44984a10a19d1022af26
pipols <pipols2007@gmail.com>
2020-06-07 18:03:54 +0300

    add reselect

--- src/selector.js
+++ src/selector.js
@@ -1,2 +1,5 @@
+import { getCardsByFilter } from "./utils/filter";
+import { getCardsBySortType } from "./utils/sort";
+
 export const getFilms = (state) => state.films;
 export const getOpenedFilm = (state) => state.openedFilm;
