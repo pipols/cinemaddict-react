034936eaab11f366b152e9d716ba06df20119068
pipols <pipols2007@gmail.com>
2020-06-07 19:44:04 +0300

    логика переключения фильтра в меню

--- src/components/main-navigation/main-navigation.jsx
+++ src/components/main-navigation/main-navigation.jsx
@@ -2,32 +2,56 @@ import React from "react";
 import { getCardsByFilter } from "../../utils/filter";
 import { FilterType } from "../../const";
 import { connect } from "react-redux";
-import { getFilms } from "../../selector";
+import { getFilms, getActiveFilter } from "../../selector";
+import { ActionCreator } from "../../reducer";
+
+const ACTIVE_CLASS_NAME = `main-navigation__item--active`;
 
 // eslint-disable-next-line react/prop-types
-const MainNavigation = ({ films }) => {
+const MainNavigation = ({ films, activeFilter, setActiveFilter }) => {
   return (
     <nav className="main-navigation">
       <div className="main-navigation__items">
         <a
+          onClick={() => setActiveFilter(FilterType.ALL)}
           href="#all"
-          className="main-navigation__item main-navigation__item--active"
+          className={`main-navigation__item ${
+            activeFilter === FilterType.ALL ? ACTIVE_CLASS_NAME : ``
+          }`}
         >
           All movies
         </a>
-        <a href="#watchlist" className="main-navigation__item">
+        <a
+          onClick={() => setActiveFilter(FilterType.WATCHLIST)}
+          href="#watchlist"
+          className={`main-navigation__item ${
+            activeFilter === FilterType.WATCHLIST ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
           Watchlist{" "}
           <span className="main-navigation__item-count">
             {getCardsByFilter(films, FilterType.WATCHLIST).length}
           </span>
         </a>
-        <a href="#history" className="main-navigation__item">
+        <a
+          onClick={() => setActiveFilter(FilterType.HISTORY)}
+          href="#history"
+          className={`main-navigation__item ${
+            activeFilter === FilterType.HISTORY ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
           History{" "}
           <span className="main-navigation__item-count">
             {getCardsByFilter(films, FilterType.HISTORY).length}
           </span>
         </a>
-        <a href="#favorites" className="main-navigation__item">
+        <a
+          onClick={() => setActiveFilter(FilterType.FAVORITES)}
+          href="#favorites"
+          className={`main-navigation__item ${
+            activeFilter === FilterType.FAVORITES ? ACTIVE_CLASS_NAME : ``
+          }`}
+        >
           Favorites{" "}
           <span className="main-navigation__item-count">
             {getCardsByFilter(films, FilterType.FAVORITES).length}
@@ -43,6 +67,13 @@ const MainNavigation = ({ films }) => {
 
 const mapStateToProps = (state) => ({
   films: getFilms(state),
+  activeFilter: getActiveFilter(state),
 });
 
-export default connect(mapStateToProps)(MainNavigation);
+const mapDispatchToProps = (dispatch) => ({
+  setActiveFilter(filter) {
+    dispatch(ActionCreator.setFilterType(filter));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainNavigation);
